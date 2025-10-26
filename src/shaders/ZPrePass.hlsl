61d9ab79a55c2e9b06e9e33615f98754caf9f557
efim <lantsmanefim@gmail.com>
2025-10-26 18:22:18 +0100

    zprepass alphatest

--- src/shaders/ZPrePass.hlsl
+++ src/shaders/ZPrePass.hlsl
@@ -3,14 +3,19 @@ cbuffer ConstantBuffer : register(b0)
 	float4x4 modelViewProjection;
 };
 
+Texture2D albedoTexture : register(t0);
+SamplerState samplerState : register(s0);
+
 struct VertexInput
 {
 	float3 position : POSITION;
+	float2 texCoord : TEXCOORD;
 };
 
 struct VertexOutput
 {
 	float4 position : SV_POSITION;
+	float2 texCoord : TEXCOORD0;
 };
 
 
@@ -18,6 +23,15 @@ VertexOutput VS(VertexInput input)
 {
 	VertexOutput output;
 	output.position = mul(float4(input.position, 1.0f), modelViewProjection);
+	output.texCoord = input.texCoord;
 	return output;
+}
+
+// Pixel shader for alpha testing
+void PS(VertexOutput input)
+{
+	float4 albedo = albedoTexture.Sample(samplerState, input.texCoord);
+	if (albedo.a < 0.5)
+		discard;
 }
 
