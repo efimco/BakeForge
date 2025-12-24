87d4db111ac149962e2ff4d3e22e2a871309c461
efim <lantsmanefim@gmail.com>
2025-12-24 15:49:41 +0100

    Linting Undo\Redo module

--- src/commands/nodeSnapshot.hpp
+++ src/commands/nodeSnapshot.hpp
@@ -9,44 +9,44 @@ class Scene;
 namespace Snapshot
 {
 
-// This command would save a snapshot of the scene node.
-// This allows you to encompass arbitrary property changes.
-// Be careful, as it might be excessively expensive to do this for some node types.
-class SceneNodeCopy final : public SnapshotBase
-{
-public:
-    SceneNodeCopy(Scene* inScene, SceneNode* inSceneNode);
+	// This command would save a snapshot of the scene node.
+	// This allows you to encompass arbitrary property changes.
+	// Be careful, as it might be excessively expensive to do this for some node types.
+	class SceneNodeCopy final : public SnapshotBase
+	{
+	public:
+		SceneNodeCopy(Scene* inScene, SceneNode* inSceneNode);
 
-protected:
-    virtual std::unique_ptr<CommandBase> exec() override;
-    virtual bool merge(SnapshotBase* command) override;
-    virtual bool containsChanges() const override;
-    virtual void onCommitTransaction() override;
+	protected:
+		virtual std::unique_ptr<CommandBase> exec() override;
+		virtual bool merge(SnapshotBase* command) override;
+		virtual bool containsChanges() const override;
+		virtual void onCommitTransaction() override;
 
-    // TO-DO: switch from pointers to handles -
-    // this would prevent them from being invalidated,
-    // which would allow you to keep
-    Scene* m_scene = nullptr;
-    SceneNode* m_sceneNode = nullptr;
-    std::unique_ptr<SceneNode> m_sceneNodeClone = nullptr;
-};
+		// TO-DO: switch from pointers to handles -
+		// this would prevent them from being invalidated,
+		// which would allow you to keep
+		Scene* m_scene = nullptr;
+		SceneNode* m_sceneNode = nullptr;
+		std::unique_ptr<SceneNode> m_sceneNodeClone = nullptr;
+	};
 
-// This command would only save a snapshot of the scene node transform.
-class SceneNodeTransform final : public SnapshotBase
-{
-public:
-    SceneNodeTransform(Scene* inScene, SceneNode* inSceneNode);
+	// This command would only save a snapshot of the scene node transform.
+	class SceneNodeTransform final : public SnapshotBase
+	{
+	public:
+		SceneNodeTransform(Scene* inScene, SceneNode* inSceneNode);
 
-protected:
-    virtual std::unique_ptr<CommandBase> exec() override;
-    virtual bool merge(SnapshotBase* command) override;
-    virtual bool containsChanges() const override;
-    virtual void onCommitTransaction() override;
+	protected:
+		virtual std::unique_ptr<CommandBase> exec() override;
+		virtual bool merge(SnapshotBase* command) override;
+		virtual bool containsChanges() const override;
+		virtual void onCommitTransaction() override;
 
-    // TO-DO: switch from pointers to handles
-    Scene* m_scene = nullptr;
-    SceneNode* m_sceneNode = nullptr;
-    Transform m_savedTransform;
-};
+		// TO-DO: switch from pointers to handles
+		Scene* m_scene = nullptr;
+		SceneNode* m_sceneNode = nullptr;
+		Transform m_savedTransform;
+	};
 
 };
--- src/commands/scopedTransaction.hpp
+++ src/commands/scopedTransaction.hpp
@@ -11,25 +11,26 @@ class CommandManager;
 class ScopedTransaction
 {
 public:
-    ScopedTransaction(CommandManager* commandManager, std::unique_ptr<SnapshotBase>&& inSnapshot);
-    ~ScopedTransaction();
+	ScopedTransaction(CommandManager* commandManager, std::unique_ptr<SnapshotBase>&& inSnapshot);
+	~ScopedTransaction();
 
-    ScopedTransaction(const ScopedTransaction&) = delete;
-    ScopedTransaction(ScopedTransaction&&) = delete;
-    ScopedTransaction& operator =(const ScopedTransaction&) = delete;
-    ScopedTransaction& operator =(ScopedTransaction&&) = delete;
+	ScopedTransaction(const ScopedTransaction&) = delete;
+	ScopedTransaction(ScopedTransaction&&) = delete;
+	ScopedTransaction& operator =(const ScopedTransaction&) = delete;
+	ScopedTransaction& operator =(ScopedTransaction&&) = delete;
 
 private:
-    std::unique_ptr<SnapshotBase> m_command;
-    CommandManager* m_commandManager;
+	std::unique_ptr<SnapshotBase> m_command;
+	CommandManager* m_commandManager;
 };
 
 template<typename TCommand>
 class TScopedTransaction : public ScopedTransaction
 {
 public:
-    template<typename ... TArgs>
-    TScopedTransaction(CommandManager* commandManager, Scene* scene, TArgs&&... args)
-        : ScopedTransaction(commandManager, std::make_unique<TCommand>(scene, std::forward<TArgs>(args)...))
-    {}
+	template<typename ... TArgs>
+	TScopedTransaction(CommandManager* commandManager, Scene* scene, TArgs&&... args)
+		: ScopedTransaction(commandManager, std::make_unique<TCommand>(scene, std::forward<TArgs>(args)...))
+	{
+	}
 };
