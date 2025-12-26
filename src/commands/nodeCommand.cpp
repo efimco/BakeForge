b701fc4e6b23b53dad376a621c80d475d4a16740
oreyg <chouthe25@gmail.com>
2025-12-26 05:01:45 +0100

    Formatting - indent in namespaces

--- src/commands/nodeCommand.cpp
+++ src/commands/nodeCommand.cpp
@@ -7,42 +7,42 @@
 namespace Command
 {
 
-DuplicateSceneNode::DuplicateSceneNode(
-	Scene* inScene,
-	SceneNode* inSceneNode,
-	bool reuseNodeHandle)
-	: m_scene(inScene)
-	, m_nodeHandle(reuseNodeHandle ? inScene->findHandleOfNode(inSceneNode) : SceneNodeHandle::invalidHandle())
-	, m_validateName(!reuseNodeHandle)
-{
-	assert(inSceneNode);
-	m_sceneNodeClone = inSceneNode->clone();
-}
-
-std::unique_ptr<CommandBase> DuplicateSceneNode::exec()
-{
-	if (m_validateName)
+	DuplicateSceneNode::DuplicateSceneNode(
+		Scene* inScene,
+		SceneNode* inSceneNode,
+		bool reuseNodeHandle)
+		: m_scene(inScene)
+		, m_nodeHandle(reuseNodeHandle ? inScene->findHandleOfNode(inSceneNode) : SceneNodeHandle::invalidHandle())
+		, m_validateName(!reuseNodeHandle)
 	{
-		m_scene->validateName(m_sceneNodeClone.get());
+		assert(inSceneNode);
+		m_sceneNodeClone = inSceneNode->clone();
 	}
-	SceneNode* clonedNode = m_scene->adoptClonedNode(std::move(m_sceneNodeClone), m_nodeHandle);
-	return std::make_unique<RemoveSceneNode>(m_scene, clonedNode);
-}
 
-RemoveSceneNode::RemoveSceneNode(Scene* inScene, SceneNode* inSceneNode)
-	: m_scene(inScene)
-	, m_nodeHandle(inScene->findHandleOfNode(inSceneNode))
-{
-	assert(inSceneNode);
-}
+	std::unique_ptr<CommandBase> DuplicateSceneNode::exec()
+	{
+		if (m_validateName)
+		{
+			m_scene->validateName(m_sceneNodeClone.get());
+		}
+		SceneNode* clonedNode = m_scene->adoptClonedNode(std::move(m_sceneNodeClone), m_nodeHandle);
+		return std::make_unique<RemoveSceneNode>(m_scene, clonedNode);
+	}
 
-std::unique_ptr<CommandBase> RemoveSceneNode::exec()
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	auto createCommand = std::make_unique<DuplicateSceneNode>(m_scene, sceneNode, true);
-	m_scene->deleteNode(sceneNode);
-	return createCommand;
-}
+	RemoveSceneNode::RemoveSceneNode(Scene* inScene, SceneNode* inSceneNode)
+		: m_scene(inScene)
+		, m_nodeHandle(inScene->findHandleOfNode(inSceneNode))
+	{
+		assert(inSceneNode);
+	}
+
+	std::unique_ptr<CommandBase> RemoveSceneNode::exec()
+	{
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		auto createCommand = std::make_unique<DuplicateSceneNode>(m_scene, sceneNode, true);
+		m_scene->deleteNode(sceneNode);
+		return createCommand;
+	}
 
 }
--- src/commands/nodeSnapshot.cpp
+++ src/commands/nodeSnapshot.cpp
@@ -7,101 +7,101 @@
 namespace Snapshot
 {
 
-SceneNodeCopy::SceneNodeCopy(Scene* inScene, SceneNode* inSceneNode)
-	: m_scene(inScene)
-	, m_nodeHandle(inScene->findHandleOfNode(inSceneNode))
-{
-	assert(inSceneNode);
-	m_sceneNodeClone = inSceneNode->clone();
-}
-
-std::unique_ptr<CommandBase> SceneNodeCopy::exec()
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	auto redoTransaction = std::make_unique<SceneNodeCopy>(m_scene, sceneNode);
-	sceneNode->copyFrom(*m_sceneNodeClone);
-	return redoTransaction;
-}
-
-bool SceneNodeCopy::merge(SnapshotBase* command)
-{
-	if (getAllowMerging() && command->getAllowMerging())
+	SceneNodeCopy::SceneNodeCopy(Scene* inScene, SceneNode* inSceneNode)
+		: m_scene(inScene)
+		, m_nodeHandle(inScene->findHandleOfNode(inSceneNode))
 	{
-		if (auto* castCommand = dynamic_cast<SceneNodeCopy*>(command))
+		assert(inSceneNode);
+		m_sceneNodeClone = inSceneNode->clone();
+	}
+
+	std::unique_ptr<CommandBase> SceneNodeCopy::exec()
+	{
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		auto redoTransaction = std::make_unique<SceneNodeCopy>(m_scene, sceneNode);
+		sceneNode->copyFrom(*m_sceneNodeClone);
+		return redoTransaction;
+	}
+
+	bool SceneNodeCopy::merge(SnapshotBase* command)
+	{
+		if (getAllowMerging() && command->getAllowMerging())
 		{
-			if (castCommand->m_nodeHandle == m_nodeHandle)
+			if (auto* castCommand = dynamic_cast<SceneNodeCopy*>(command))
 			{
-				m_sceneNodeClone = std::move(castCommand->m_sceneNodeClone);
-				castCommand->m_sceneNodeClone = nullptr;
-				return true;
+				if (castCommand->m_nodeHandle == m_nodeHandle)
+				{
+					m_sceneNodeClone = std::move(castCommand->m_sceneNodeClone);
+					castCommand->m_sceneNodeClone = nullptr;
+					return true;
+				}
 			}
 		}
+		return false;
 	}
-	return false;
-}
 
-bool SceneNodeCopy::containsChanges() const
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	return m_sceneNodeClone->differsFrom(*sceneNode);
-}
-
-void SceneNodeCopy::onCommitTransaction()
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	sceneNode->onCommitTransaction(*m_scene);
-}
-
-SceneNodeTransform::SceneNodeTransform(Scene* inScene, SceneNode* inSceneNode)
-	: m_scene(inScene)
-	, m_nodeHandle(inScene->findHandleOfNode(inSceneNode))
-{
-	m_savedTransform = inSceneNode->transform;
-}
-
-std::unique_ptr<CommandBase> SceneNodeTransform::exec()
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	auto redoTransaction = std::make_unique<SceneNodeTransform>(m_scene, sceneNode);
-	sceneNode->transform = m_savedTransform;
-	return redoTransaction;
-}
-
-bool SceneNodeTransform::merge(SnapshotBase* command)
-{
-	if (getAllowMerging() && command->getAllowMerging())
+	bool SceneNodeCopy::containsChanges() const
 	{
-		if (const auto* castCommand = dynamic_cast<SceneNodeTransform*>(command))
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		return m_sceneNodeClone->differsFrom(*sceneNode);
+	}
+
+	void SceneNodeCopy::onCommitTransaction()
+	{
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		sceneNode->onCommitTransaction(*m_scene);
+	}
+
+	SceneNodeTransform::SceneNodeTransform(Scene* inScene, SceneNode* inSceneNode)
+		: m_scene(inScene)
+		, m_nodeHandle(inScene->findHandleOfNode(inSceneNode))
+	{
+		m_savedTransform = inSceneNode->transform;
+	}
+
+	std::unique_ptr<CommandBase> SceneNodeTransform::exec()
+	{
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		auto redoTransaction = std::make_unique<SceneNodeTransform>(m_scene, sceneNode);
+		sceneNode->transform = m_savedTransform;
+		return redoTransaction;
+	}
+
+	bool SceneNodeTransform::merge(SnapshotBase* command)
+	{
+		if (getAllowMerging() && command->getAllowMerging())
 		{
-			if (castCommand->m_nodeHandle == m_nodeHandle)
+			if (const auto* castCommand = dynamic_cast<SceneNodeTransform*>(command))
 			{
-				m_savedTransform = castCommand->m_savedTransform;
-				return true;
+				if (castCommand->m_nodeHandle == m_nodeHandle)
+				{
+					m_savedTransform = castCommand->m_savedTransform;
+					return true;
+				}
 			}
 		}
+		return false;
 	}
-	return false;
-}
 
-bool SceneNodeTransform::containsChanges() const
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	return
-		sceneNode->transform.position != m_savedTransform.position ||
-		sceneNode->transform.rotation != m_savedTransform.rotation ||
-		sceneNode->transform.scale != m_savedTransform.scale;
-}
+	bool SceneNodeTransform::containsChanges() const
+	{
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		return
+			sceneNode->transform.position != m_savedTransform.position ||
+			sceneNode->transform.rotation != m_savedTransform.rotation ||
+			sceneNode->transform.scale != m_savedTransform.scale;
+	}
 
-void SceneNodeTransform::onCommitTransaction()
-{
-	SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
-	assert(sceneNode);
-	sceneNode->onCommitTransaction(*m_scene);
-}
+	void SceneNodeTransform::onCommitTransaction()
+	{
+		SceneNode* sceneNode = m_scene->getNodeByHandle(m_nodeHandle);
+		assert(sceneNode);
+		sceneNode->onCommitTransaction(*m_scene);
+	}
 
 }
