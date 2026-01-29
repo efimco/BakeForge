5826ee5f29a70701360a7e1e83c72568dcd1ae2f
efim <lantsmanefim@gmail.com>
2026-01-29 16:44:34 +0100

    small fix

--- src/primitive.cpp
+++ src/primitive.cpp
@@ -131,9 +131,9 @@ void Primitive::buildBVH()
 	BVH::BVHBuilder builder(m_sharedData->triangles, m_sharedData->triangleIndices);
 	m_sharedData->bvhNodes = builder.BuildBVH();
 
-	auto stats = BVH::BVHBuilder::CalculateStats(m_sharedData->bvhNodes);
-	std::cout << "BVH for: " << name << std::endl;
-	BVH::BVHBuilder::PrintStats(stats);
+	// auto stats = BVH::BVHBuilder::CalculateStats(m_sharedData->bvhNodes);
+	// std::cout << "BVH for: " << name << std::endl;
+	// BVH::BVHBuilder::PrintStats(stats);
 }
 
 
